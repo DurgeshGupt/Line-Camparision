717e563eda252a0adea366b096682b77a641a2ae
DurgeshGupt <durgeshgu106@gmail.com>
2022-05-15 09:41:37 +0530

    Added: get line equal or not

--- src/Linecampirision/linecampirision.java
+++ src/Linecampirision/linecampirision.java
@@ -3,28 +3,39 @@ import java.util.Scanner;
 
 public class linecampirision {
 
-        double line1;
-
         public static void main(String[] args)
         {
             Scanner sc = new Scanner(System.in);
-            System.out.println("Welcome to get line");
-            System.out.println("enter x1 axis");
+            System.out.println("enter firstline x1 axis");
             int x1 = sc.nextInt();
-            System.out.println("enter x2 axis");
-            int x2 = sc.nextInt();
-            System.out.println("enter y1 axis");
+            System.out.println("enter firstline x2 axis");
+            int x2 =sc.nextInt();
+            System.out.println("enter firstline y1 axis");
             int y1 = sc.nextInt();
-            System.out.println("enter y2 axis");
+            System.out.println("enter firstline y2 axis");
             int y2 = sc.nextInt();
+            System.out.println("enter secondline x1 axis");
+            int a1 = sc.nextInt();
+            System.out.println("enter secondline x2 axis");
+            int a2 =sc.nextInt();
+            System.out.println("enter secondline y1 axis");
+            int b1 = sc.nextInt();
+            System.out.println("enter secondline y2 axis");
+            int b2 = sc.nextInt();
 
+            double line1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 
-            double line1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+            double line2 = Math.sqrt((a2-a1)*(a2-a1) + (b2-b1)*(b2-b1));
 
-            System.out.println("The length of line is" + line1 );
+            if(line1 == line2)
+            {
+                System.out.println("lines are equal");
+            }
+            else
+                System.out.println("lines are not equal");
 
 
         }
-
     }
+
 
